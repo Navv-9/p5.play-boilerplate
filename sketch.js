be4ca90052981d8a61afcb84c7e6f98952bcee57
Navv-9 <53648356+Navv-9@users.noreply.github.com>
2019-08-21 16:55:42 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,9 +1,33 @@
+const Engine= Matter.Engine;
+const World= Matter.World;
+const Bodies= Matter.Bodies;
+
+var engine,world;
+var ground;
+var ball;
+
 function setup() {
   createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+  engine=Engine.create();
+  world= engine.world;
+  var options= {
+    isStatic: true
+  }
+  ground= Bodies.rectangle(200,350,400,20,options);
+World.add(world,ground);
+var ball_options= {
+  restitution: 1.0
+}
+ball= Bodies.circle(400,100,40,ball_options);
+World.add(world,ball);
+
 }
 
 function draw() {
-  background(255,255,255);  
+  background(255,255,255);
+  Engine.update(engine); 
+  rect(ground.position.x,ground.position.y,400,20) ;
+  ellipseMode(RADIUS);
+  ellipse(ball.position.x,ball.position.y,40,40);
   drawSprites();
 }
